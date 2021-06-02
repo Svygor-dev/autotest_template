ec0af602dd8f9eccdb9fe4a8b6cf38012baa58e5
Pavel Romashov <romashov@astralnalog.ru>
2021-06-02 20:04:06 +0300

    - добавил валидацию ответа по json-схеме

--- TestHelpers/validator.py
+++ TestHelpers/validator.py
@@ -1,4 +1,17 @@
+import jsonschema
+import json
+
+
 class Validator:
 
-    def validate_api_result_schema(self, result_text):
-        pass
+    def validate_api_result_schema(self, result_api):
+        with open("./Data/Schemas/api_result_schema.json") as schema_file:
+            schema = json.load(schema_file)
+        try:
+            json_file = json.loads(result_api)
+            jsonschema.validate(json_file, schema)
+            result = True
+        except Exception as e:
+            result = e
+
+        assert result is True, result
