c34d7f343777246ca77657dbec9b7dd4e1661d05
Pavel Romashov <romashov@astralnalog.ru>
2021-06-02 20:05:37 +0300

    - внёс в тестовые данные ожидаемый статус-код

--- Data/Models/smoke_case_model.py
+++ Data/Models/smoke_case_model.py
@@ -2,13 +2,15 @@ class SmokeCase:
 
     user_id = None
     test_name = None
+    expected_status_code = None
 
-    def __init__(self, dictionary=None, test_name=None, user_id=None):
+    def __init__(self, dictionary=None, test_name=None, user_id=None, expected_status_code=None):
         if dictionary is not None:
             self.__dict__ = dictionary
         else:
             self.user_id = user_id
             self.test_name = test_name
+            self.expected_status_code = expected_status_code
 
     def __str__(self):
         return self.test_name
