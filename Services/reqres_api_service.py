865421f76847226f58a1f548d97e4d664dce4a74
Pavel Romashov <romashov@astralnalog.ru>
2021-06-02 19:53:39 +0300

    - добавил реализацию синглтона в API-сервис

--- Services/reqres_api_service.py
+++ Services/reqres_api_service.py
@@ -5,6 +5,11 @@ class ReqresApiClient:
 
     url_base = "https://reqres.in"
 
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ReqresApiClient, cls).__new__(cls)
+        return cls.instance
+
     def get_user(self, user_id):
         get_user_url = self.url_base + "/api/users"
         payload = {"id": user_id}
