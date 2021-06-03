af6a2a4317b381416debbccccaaafa72e274ab22
Pavel Romashov <romashov@astralnalog.ru>
2021-06-03 11:45:24 +0300

    - добавил комменты в TestHelpers

--- TestHelpers/test_processor.py
+++ TestHelpers/test_processor.py
@@ -1,7 +1,17 @@
 class TestProcessor:
+    """
+    Используется для имитации действий пользователя.
+    В этом классе будут методы по отправке запросов, цепочек запросов, ожиданию/получению ответов
+        и другие действия, которые выполняет пользователь с тестируемой системой.
+    """
 
     reqres_api_service = None
 
     def send_request(self, user_id):
+        """
+        Метод отправки запроса по получению пользователя по user_id
+        :param user_id: id пользователя
+        :return: ответ от API
+        """
         result = self.reqres_api_service.get_user(user_id)
         return result
--- TestHelpers/validator.py
+++ TestHelpers/validator.py
@@ -3,15 +3,31 @@ import json
 
 
 class Validator:
+    """
+    Класс, содержащий различные методы проверки результатов.
+    Будут различные проверки по JSON-схемам, проверки конкретных значений в результатах и т.д.
+    """
 
     def validate_api_result_schema(self, result_api):
+        """
+        Метод проверки результата ответа по json-схеме.
+        :param result_api: текст ответа, который нужно проверить по схеме
+        """
+
+        """Загружаем json-схему из файла"""
         with open("./Data/Schemas/api_result_schema.json") as schema_file:
             schema = json.load(schema_file)
         try:
+            "Проверяем, что результат соответствует json-схеме"
             json_file = json.loads(result_api)
             jsonschema.validate(json_file, schema)
             result = True
         except Exception as e:
+            """В случае, если проверка по схеме не проходит, то оставляем """
             result = e
-
+        """
+        Проверяем, что в результате проверка по схеме успешно пройдена
+        Если нет, то в качестве текста ошибки будет отображена ошибка, которую вернул метод
+            jsonschema.validate()
+        """
         assert result is True, result
